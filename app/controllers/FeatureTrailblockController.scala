f59530a49af8c9212ee61591c9f882e93a5b251f
Grant Klopper <grant.klopper@guardian.co.uk>
2012-09-21 14:01:50 +0100

    Added management pages

--- app/controllers/FeatureTrailblockController.scala
+++ app/controllers/FeatureTrailblockController.scala
@@ -7,6 +7,7 @@ import tools.S3
 
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.json.Json.toJson
+import conf.{ConfigUpdateCounter, ConfigUpdateErrorCounter}
 
 object FeatureTrailblockController extends Controller with Logging {
 
@@ -31,9 +32,11 @@ object FeatureTrailblockController extends Controller with Logging {
   private def saveConfigOrError(json: JsValue) = try {
     S3.putConfig(Json.stringify(json))
     log.info("config successfully updated")
+    ConfigUpdateCounter.recordCount(1)
     Ok(toJson(Map("status" -> "Configuration updated")))
   } catch { case e =>
       log.error("exception saving config", e)
+      ConfigUpdateErrorCounter.recordCount(1)
       InternalServerError(toJson(Map("status" -> e.getMessage)))
   }
 }
