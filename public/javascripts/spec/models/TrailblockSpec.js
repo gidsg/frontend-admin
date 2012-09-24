7b6783a4d7cc6857f2873c69619e67fe4de7b1e6
Darren Hurley <ironsidevsquincy@googlemail.com>
2012-09-24 09:59:14 +0100

    adding trailblock update and clear test

--- public/javascripts/spec/models/TrailblockSpec.js
+++ public/javascripts/spec/models/TrailblockSpec.js
@@ -1,6 +1,20 @@
 curl(['models/trailblock', 'Knockout']).then(
 
     function  (Trailblock, Knockout) {
+
+        /**
+         * adding data provider pattern to jasmine - https://github.com/jphpsf/jasmine-data-provider
+         */
+        function using(name, values, func){
+            for (var i = 0, count = values.length; i < count; i++) {
+                if (Object.prototype.toString.call(values[i]) !== '[object Array]') {
+                    values[i] = [values[i]];
+                }
+                func.apply(this, values[i]);
+                jasmine.currentEnv_.currentSpec.description += ' (with "' + name + '" using ' + values[i].join(', ') + ')';
+            }
+        }
+
         describe('Trailblock Model', function() {
 
             var trailblock;
@@ -20,7 +34,7 @@ curl(['models/trailblock', 'Knockout']).then(
             var observableProps = {
                 'id': '',
                 'title': '',
-                'numItems' : '3',
+                'numItems' : 3,
                 'lead': true
             };
 
@@ -41,6 +55,30 @@ curl(['models/trailblock', 'Knockout']).then(
                 });
 
             }
+
+            using('trailblock data', [{'title': 'foo'}], function(data) {
+                it('update method should update trailblock with supplied data', function() {
+                    trailblock.update(data);
+                    for (var prop in data) {
+                        expect(trailblock[prop]()).toEqual(data[prop]);
+                    }
+                });
+            });
+
+            it('clear method should empty all observables', function() {
+                // add some data
+                trailblock.update({
+                    'id': 'bar',
+                    'title': 'foo',
+                    'numItems': 5,
+                });
+                // clear trialblock
+                trailblock.clear();
+                // make sure observables are all empty
+                for (var name in observableProps) {
+                    expect(trailblock[name]()).toEqual('');
+                }
+            });
 
         });
     },
