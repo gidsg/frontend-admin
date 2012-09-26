9ccb36a29978cc3654e99a025fff38fb75933e66
Darren Hurley <ironsidevsquincy@googlemail.com>
2012-09-26 16:50:54 +0100

    don't make numitems and lead props observable until necessary (they were being erased)

--- app/assets/javascripts/models/trailblock.js
+++ app/assets/javascripts/models/trailblock.js
@@ -4,8 +4,8 @@ define(['Knockout'], function (Knockout) {
 		this.type     = 'tag';
     	this.id       = Knockout.observable('');
     	this.title 	  = Knockout.observable('');
-    	this.numItems = Knockout.observable(3);
-    	this.lead 	  = Knockout.observable(true);
+    	this.numItems = 3;
+    	this.lead 	  = true;
 
         this.update = function(data)
         {
