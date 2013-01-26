481446ee6baf52ea4f8bdc92b534c99e86f232c9
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-01-26 17:59:22 +0000

    Link articles to the mDot site

--- public/javascripts/models/article.js
+++ public/javascripts/models/article.js
@@ -2,9 +2,11 @@ define(['Knockout'], function (ko) {
 
     var Article = function(opts) {
 
-        opts = opts || {};
+        var opts = opts || {},
+            mDotHost = 'http://m.guardian.co.uk/';
 
         this.id         = ko.observable(opts.id         || '');
+        this.mDot        = ko.observable(mDotHost + opts.id || '');
         this.webTitle   = ko.observable(opts.webTitle   || '');
         this.webPublicationDate = ko.observable( humanized_time_span( opts.webPublicationDate ) || '-');
         this.importance = ko.observable(opts.importance || 50);
