389b21f9ca907686d6323879af0dff6edc04df02
Stephan Fowler <stephanfowler@gmail.com>
2013-03-20 22:37:25 +0000

    Add-to-homescreen switch

--- app/controllers/SwitchboardController.scala
+++ app/controllers/SwitchboardController.scala
@@ -32,6 +32,7 @@ object SwitchboardController extends Controller with AuthLogging with Logging {
     Switch("quantcast", false, "Enable the Quantcast audience segment tracking."),
     Switch("integration-test-switch", true, "Switch that is only used while running tests. You never need to change this switch"),
     Switch("story-version-b", false, "Switch to enable version B of story page.")
+    Switch("homescreen", false, "If this switch is enabled the add-to-homescreen popup will plague iOS users.")
   )
 
   def render() = AuthAction{ request =>
