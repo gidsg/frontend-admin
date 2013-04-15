a231580fc6aa707438309026ede101e4734f4273
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-04-15 14:06:34 +0100

    Image server frontend switch

--- app/controllers/SwitchboardController.scala
+++ app/controllers/SwitchboardController.scala
@@ -34,6 +34,7 @@ object SwitchboardController extends Controller with AuthLogging with Logging {
     Switch("story-version-b", false, "Switch to enable version B of story page."),
     Switch("homescreen", false, "If this switch is enabled the add-to-homescreen popup will plague iOS users."),
     Switch("optimizely", false, "If this switch is on Optimizely will be enabled."),
+    Switch("image-server", false, "If this switch is on images will be served off i.guim.co.uk (dynamic image host)"),
     Switch("adverts", true, "If this switch is on then OAS adverts will be loaded with JavaScript")
   )
 
