1706bfe69042da4cfff7f1a9920621b5e252ec17
Stephan Fowler <stephan.fowler@guardian.co.uk>
2013-05-03 14:50:58 +0100

    Add switch for swipe navigation

--- app/controllers/SwitchboardController.scala
+++ app/controllers/SwitchboardController.scala
@@ -39,7 +39,8 @@ object SwitchboardController extends Controller with AuthLogging with Logging {
     Switch("video-adverts", false, "If this switch is on then OAS video adverts will be loaded with JavaScript"),
     Switch("ab-related-content-v2", false, "If this switch is on related content AB test will be enabled."),
     Switch("ab-local-election-story-v2", false, "If this switch is on local election story AB test will be enabled."),
-    Switch("css-lazy-load", false, "If this switch is on then use a slimmed down, lazy-loaded css setup (css in head with server sepcific files)")
+    Switch("css-lazy-load", false, "If this switch is on then use a slimmed down, lazy-loaded css setup (css in head with server sepcific files)"),
+    Switch("swipe-navigation", false, "If this switch is on then swipe navigation is enabled")
   )
 
   def render() = AuthAction{ request =>
